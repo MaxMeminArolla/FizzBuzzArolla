510d13a0a962528c0d5df028142a8840277015bc
MEMIN <maxmemin@hotmail.com>
2022-01-11 12:29:23 +0100

    zero linter messages

--- FizzBuzzArolla/Kata.cs
+++ FizzBuzzArolla/Kata.cs
@@ -1,23 +1,21 @@
-﻿using System;
-
-namespace FizzBuzzArolla
+﻿namespace FizzBuzzArolla
 {
     public class Kata
     {
         public static string FizzBuzz(int number)
         {
-            if (isDivisibleBy3(number) && isDivisibleBy5(number)) return "FizzBuzz";
-            if (isDivisibleBy3(number)) return "Fizz";
-            if (isDivisibleBy5(number)) return "Buzz";
+            if (IsDivisibleBy3(number) && IsDivisibleBy5(number)) return "FizzBuzz";
+            if (IsDivisibleBy3(number)) return "Fizz";
+            if (IsDivisibleBy5(number)) return "Buzz";
             return number.ToString();
         }
 
-        private static bool isDivisibleBy3(int number)
+        private static bool IsDivisibleBy3(int number)
         {
             return number % 3 == 0;
         }
 
-        private static bool isDivisibleBy5(int number)
+        private static bool IsDivisibleBy5(int number)
         {
             return number % 5 == 0;
         }
--- FizzBuzzTest/UnitTest1.cs
+++ FizzBuzzTest/UnitTest1.cs
@@ -1,4 +1,3 @@
-using System;
 using Xunit;
 using FizzBuzzArolla;
 
@@ -7,25 +6,25 @@ namespace FizzBuzzTest
     public class UnitTest1
     {
         [Fact]
-        public void should_return_fizz_with_number_3()
+        public void Should_return_fizz_with_number_3()
         {
             Assert.Equal("Fizz", Kata.FizzBuzz(3));
         }
 
         [Fact]
-        public void should_return_buzz_with_number_5()
+        public void Should_return_buzz_with_number_5()
         {
             Assert.Equal("Buzz", Kata.FizzBuzz(5));
         }
 
         [Fact]
-        public void should_return_FizzBuzz_with_number_15()
+        public void Should_return_FizzBuzz_with_number_15()
         {
             Assert.Equal("FizzBuzz", Kata.FizzBuzz(15));
         }
 
         [Fact]
-        public void should_return_1_with_number_1()
+        public void Should_return_1_with_number_1()
         {
             Assert.Equal("1", Kata.FizzBuzz(1));
         }
