011303ef2b12922a9a535676ca83419314c82e96
MEMIN <maxmemin@hotmail.com>
2022-01-11 12:23:09 +0100

    all tests passed

--- FizzBuzzArolla/Kata.cs
+++ FizzBuzzArolla/Kata.cs
@@ -6,10 +6,10 @@ namespace FizzBuzzArolla
     {
         public static string FizzBuzz(int number)
         {
-            if (number == 5) return "Buzz";
-            if (number == 15) return "FizzBuzz";
-            if (number == 1) return "1";
-            return "Fizz";
+            if (number % 3 == 0 && number % 5 == 0) return "FizzBuzz";
+            if (number % 3 == 0) return "Fizz";
+            if (number % 5 == 0) return "Buzz";
+            return number.ToString();
         }
     }
 }
